d3a331d91e5fda0ed79c1db078dc89bce1c993ba
ryanluo <ryanluo42@gmail.com>
2021-05-31 22:50:21 -0700

    User gets to choose a stock

--- picker.py
+++ picker.py
@@ -5,10 +5,10 @@ import pandas as pd
 # Remember to install/click Certificate.command and Shell command located in Python 3.9 file through MacOS-Applications
 #
 #
-ticker = 'FB'
+ticker = input("Type in a Stock Symbol: ")
 period1 = int(time.mktime(datetime.datetime(2021, 5, 1, 23, 59).timetuple()))
 period2 = int(time.mktime(datetime.datetime(2021, 5, 30, 23, 59).timetuple()))
-interval = '1wk'
+interval = '1d'
 
 query_string = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true"
 
