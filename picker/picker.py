c35fe4c7d5ba0b2a4dc411b2166d9873f1b2c69b
ryanluo <ryanluo42@gmail.com>
2021-06-20 22:51:06 -0700

    Finished the front end of stocks page, will look to include a stock graph

--- picker/picker.py
+++ picker/picker.py
@@ -8,12 +8,14 @@ import pandas as pd
 # Created a function for ticker, and needed to return df while also printing df to ensure functionality
 def picker_select(ticker):
     period1 = int(time.mktime(datetime.datetime(2021, 5, 1, 23, 59).timetuple()))
-    period2 = int(time.mktime(datetime.datetime(2022, 6, 13, 23, 59).timetuple()))
+    period2 = int(time.mktime(datetime.datetime(2022, 6, 19, 23, 59).timetuple()))
     interval = '1d'
 
     query_string = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true"
 
     df = pd.read_csv(query_string)
+
+
     print(df)
     return df
 
