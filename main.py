40cc5bf0ebb0e43f9914d691164907f94c9bc342
Colin Szeto <kolinszeto@gmail.com>
2021-06-20 21:27:49 -0700

    added in functionality to take in a ticker and display it onto the front end

--- main.py
+++ main.py
@@ -14,8 +14,53 @@ def home():
 def picker():
     if request.form:
         ticker_value = request.form.get("ticker")
-        print(ticker_value)
-        return render_template("picker.html", df=picker_select(ticker_value))
+        # print(ticker_value)
+
+        df = picker_select(ticker_value)
+
+        # creating a list for each column in the csv format
+        date_list = []
+        for data in df["Date"]:
+            date_list.append(data)
+
+        open_list = []
+        for data in df["Open"]:
+            open_list.append(data)
+
+        high_list = []
+        for data in df["High"]:
+            high_list.append(data)
+
+        close_list = []
+        for data in df["Close"]:
+            close_list.append(data)
+
+        adj_close_list = []
+        for data in df["Adj Close"]:
+            adj_close_list.append(data)
+
+        volume_list = []
+        for data in df["Volume"]:
+            volume_list.append(data)
+        # end of creating a list for each column
+
+        # creating dictionaries from specific items for each list previously created
+        every_info = []
+        b = 0
+        for i in date_list:
+            new_dict = {
+                "Date":date_list[b],
+                "Open":open_list[b],
+                "High":high_list[b],
+                "Close":close_list[b],
+                "Adj Close":adj_close_list[b],
+                "Volume":volume_list[b]
+            }
+            every_info.append(new_dict)
+            b = b + 1
+        print(every_info)
+
+        return render_template("picker.html", every_info=every_info)
     return render_template("picker.html")
 
 
